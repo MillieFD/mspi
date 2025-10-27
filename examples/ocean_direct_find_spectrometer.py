2a63e1f74b3f7537de786e6bfaa3d798d32f1224
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-10-27 14:43:46 +0000

    Improved error handling in `ocean_direct_find_spectrometer.py` and updated script documentation.

--- examples/ocean_direct_find_spectrometer.py
+++ examples/ocean_direct_find_spectrometer.py
@@ -1,5 +1,16 @@
 """
+This example script demonstrates how to use the Ocean Direct library. Requires a connected Ocean Insight optical
+spectrometer. See https://www.oceanoptics.com/software/ for more information.
 
+The program:
+    1. Initialise the Ocean Direct library
+    2. Establishes connection with the spectrometer
+    3. Reports information from the spectrometer
+    4. Handles alerts and disconnection events
+
+Raises:
+    ValueError: If no Ocean Direct devices are detected
+    IOError: If serial number / firmware version / FPGA version cannot be read from the spectrometer
 """
 
 from OceanDirect import OceanDirectAPI, OceanDirectError
@@ -17,8 +28,7 @@ print(f"API Version : {api_version[0]}.{api_version[1]}.{api_version[2]}")
 
 # Search for connected devices
 if not od.find_usb_devices() > 0:
-    print("No devices found.")
-    exit(0)
+    raise ValueError("No devices found.")
 
 # Get the device ID list
 device_ids = od.get_device_ids()
@@ -38,24 +48,24 @@ try:
     serial_number = device.get_serial_number()
     print(f"Serial Number : {serial_number}")
 except OceanDirectError as err:
-    # Catch error
-    print(f"Serial Number Error : {err.get_error_details()}")
+    # Couldn't get serial number
+    raise IOError(f"Serial Number Error : {err.get_error_details()}")
 
 try:
     # Firmware version
     firmware_version = device.Advanced.get_revision_firmware()
     print(f"Firmware Version : {firmware_version}")
 except OceanDirectAPI.OceanDirectError as err:
-    # Catch error
-    print(f"Firmware Version Error : {err.get_error_details()}")
+    # Couldn't get firmware version
+    raise IOError(f"Firmware Version Error : {err.get_error_details()}")
 
 try:
     # FPGA version
     fpga_version = device.Advanced.get_revision_fpga()
     print(f"FPGA Version : {fpga_version}")
 except OceanDirectError as err:
-    # Catch error
-    print(f"FPGA Version Error : {err.get_error_details()}")
+    # Couldn't get FPGA version
+    raise IOError(f"FPGA Version Error : {err.get_error_details()}")
 
 # SHUTDOWN
 
