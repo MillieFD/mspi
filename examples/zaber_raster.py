09b504ef1b042ffc2ee2e782698634067b4604bd
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-02-05 13:09:23 +0000

    Updated `zaber_raster.py` example: clarified device requirements, adjusted formatting, and improved code consistency.

--- examples/zaber_raster.py
+++ examples/zaber_raster.py
@@ -1,6 +1,6 @@
 """
-This example script demonstrates how to use the Zaber Motion library. Requires a connected Zaber motorized XY stage.
-See https://software.zaber.com/motion-library/ for more information.
+This example script demonstrates how to use the Zaber Motion library. Requires a connected Zaber motorized stage with
+two or more axes. See https://software.zaber.com/motion-library/ for more information.
 
 The program:
     1. Establishes connection with the stage
@@ -15,7 +15,6 @@ Raises:
 from zaber_motion.ascii import Connection
 
 with Connection.open_serial_port("dev/tty/USB0") as connection:
-
     # OPTIONAL: Subscribing to alerts
 
     # When the device raises an alert, Zaber Motion invokes the corresponding event.
@@ -24,10 +23,10 @@ with Connection.open_serial_port("dev/tty/USB0") as connection:
     # 3. Zaber Motion will execute the lambda fn each time the event occurs (e.g. to handle the alert)
 
     # Define a lambda fn to print alerts
-    connection.alert.subscribe(lambda alert: print('Alert:', alert))
+    connection.alert.subscribe(lambda alert: print("Alert:", alert))
 
     # Define a lambda fn to raise an error if the device disconnects
-    connection.disconnected.subscribe(lambda err: print('Disconnected:', err))
+    connection.disconnected.subscribe(lambda err: print("Disconnected:", err))
 
     # Enable alerts for all devices on the connection.
     connection.enable_alerts()
@@ -39,7 +38,7 @@ with Connection.open_serial_port("dev/tty/USB0") as connection:
 
     try:
         # One device found i.e. len(device_list) = 1 SUCCESS
-        device, = device_list
+        (device,) = device_list
     except ValueError:
         # No devices found i.e. len(device_list) = 0 OR
         # Multiple devices found i.e. len(device_list) ≥ 1
@@ -60,16 +59,16 @@ with Connection.open_serial_port("dev/tty/USB0") as connection:
     # PARAMETERS: The system will scan a square area defined by two corners (start & end)
 
     # Define the starting coordinates
-    x_start = 0 # mm
-    y_start = 0 # mm
+    x_start = 0  # mm
+    y_start = 0  # mm
 
     # Define the end coordinates
-    x_end = 100 # mm
-    y_end = 100 # mm
+    x_end = 100  # mm
+    y_end = 100  # mm
 
     # Define the scan speed & step size between adjacent rows
-    speed = 1 # mm/s
-    step = 10 # mm
+    speed = 1  # mm/s
+    step = 10  # mm
 
     # RASTER: Move in a raster scan pattern
 
@@ -78,7 +77,6 @@ with Connection.open_serial_port("dev/tty/USB0") as connection:
     y.move_absolute(position=y_start, unit="mm")
 
     while x.get_position() <= x_end:
-
         # Move the y-axis from START to END coordinates
         while y_start <= y.get_position() <= y_end:
             # Move the y-axis at the specified speed
